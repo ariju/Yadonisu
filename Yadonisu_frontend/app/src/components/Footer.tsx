17e0ddc3f543fe8122a5007fab00db22ed0843d5
junichi.ariyama <junichi.ariyama@appirits.com>
2023-12-26 19:07:33 +0900

    ヘッダーフッター作成

---
+++ Yadonisu_frontend/app/src/components/Footer.tsx
@@ -0,0 +1,17 @@
+/* eslint-disable import/no-unresolved */
+import React from 'react'
+import { FaRegCopyright } from 'react-icons/fa'
+import styles from '@/styles/Footer.module.scss'
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.footerCopyright}>
+        <FaRegCopyright />
+        <p>Copyright © 2023 Your Company. All Rights Reserved.</p>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ Yadonisu_frontend/app/src/components/Header.tsx
@@ -0,0 +1,59 @@
+/* eslint-disable import/no-unresolved */
+import React from 'react'
+import { FiAlignJustify, FiGlobe } from 'react-icons/fi'
+import { IoIosSearch } from 'react-icons/io'
+import { SiAirbnb } from 'react-icons/si'
+import styles from '@/styles/Header.module.scss'
+
+type HeaderProps = {
+  title: string
+}
+
+const Header = ({}: HeaderProps) => {
+  return (
+    <header className={styles._wrapper}>
+      <div className={styles._inner}>
+        <div className={styles._logo}>
+          <SiAirbnb className={styles._icon_airbub} />
+          <p className={styles._logo_title}>airbnb</p>
+        </div>
+
+        <ul className={styles._list_center}>
+          <li className={styles._list_item}>
+            <button className={styles._list_button_left}>どこでも</button>
+          </li>
+          <li className={`${styles._vertical_line} ${styles._list_item}`}>
+            <button className={styles._list_button_center}>週の指定なし</button>
+          </li>
+          <li className={styles._list_item}>
+            <button className={styles._list_button_right}>ゲスト数を追加</button>
+            <div className={styles._red_circle}>
+              <IoIosSearch className={styles._search_icon} />
+            </div>
+          </li>
+        </ul>
+
+        <div className={styles._list_right}>
+          <ul className={styles._list_right}>
+            <li className={styles._list_right_item}>
+              <button className={styles._list_right_button}>Airbubにお部屋を掲載</button>
+            </li>
+            <li className={styles._list_right_item}>
+              <FiGlobe className={styles._list_right_language_icon} />
+            </li>
+            <div className={styles._list_right_group}>
+              <li className={styles._list_right_item}>
+                <FiAlignJustify className={styles._list_right_icon} />
+              </li>
+              <li className={styles._list_right_item}>
+                <FiAlignJustify className={styles._list_right_icon} />
+              </li>
+            </div>
+          </ul>
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default Header
